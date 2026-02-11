d1ae6e2db6aaf8a3f196ead0a281105fd52f98f8
reeta2208 <reeta@telus.com>
2026-02-11 14:12:32 +0530

    commit

---
+++ Github_Eclipse/src/sample/AreaOfTriangle.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class AreaOfTriangle {
+	
+	public static void area()
+	{
+		int b=29;
+		int h=3;
+		double a=(b*h)*1/2;
+		System.out.println("Area is "+ a);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		area();
+	}
+
+}
